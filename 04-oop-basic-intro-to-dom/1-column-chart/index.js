318df110fc0e0a16649cd82ea40e430d48c06f0b
VadimBolokhov <wad4@inbox.ru>
2021-05-17 20:09:29 +0300

    ColumnChart компонент

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,82 @@
 export default class ColumnChart {
+  constructor({label = '', data = [], value = 0, link = '', formatHeading} = {}) {
+    this.label = label;
+    this.link = link;
+    this.value = value;
+    this.data = data;
+    this.formatHeading = formatHeading;
+    this.chartHeight = 50;
+    this.render();
+  }
 
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.getTemplate();
+
+    this.element = element.firstElementChild;
+
+    this.subElements = this.getSubElements(this.element);
+  }
+
+  getTemplate() {
+    return `
+      <div class="column-chart ${this.data.length ? '' : 'column-chart_loading'}" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total ${this.label} ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">
+            ${this.getFormattedValue(this.value)}
+          </div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnBody(this.data)}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+  }
+
+  getFormattedValue(value) {
+    return this.formatHeading !== undefined ? this.formatHeading(value) : value;
+  }
+
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    return data.map(item => {
+      const percent = (item / maxValue * 100).toFixed(0);
+
+      return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+    }).join('');
+  }
+
+  getSubElements(element) {
+    let elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  update(newData) {
+    this.subElements.body.innerHTML = this.getColumnBody(newData);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
 }
