72adf4a99875c7bd85783c5de80df91421658e24
VadimBolokhov <wad4@inbox.ru>
2021-05-20 20:02:50 +0300

    NotificationMessage компонент

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,50 @@
 export default class NotificationMessage {
+  static activeNotification;
 
+  constructor(message = '', {duration = 0, type = 'success'} = {}) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.durationInSeconds = (duration / 1000) + 's';
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `<div class="notification ${this.type}" style="--value:${this.durationInSeconds}">
+        <div class="timer">
+            <div class="inner-wrapper">
+                <div class="notification-header">Notification</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+    </div>`;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+    NotificationMessage.activeNotification = this.element;
+  }
+
+  show(parent = document.body) {
+    parent.append(this.element);
+    setTimeout(() => {this.remove()}, this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    NotificationMessage.activeNotification = null;
+  }
 }
